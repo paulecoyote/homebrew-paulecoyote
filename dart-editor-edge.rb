5fb61f68d78058b7f46b0cc640f818c152494c5b
Paul Evans (Deploy) <paul.d.evans+dedeploy@gmail.com>
2014-03-12 17:00:21 +0000

    DartEditorDev now version 1.3.0-dev.3.2 (revision 33495) DartEditorEdge now version 1.3.0-edge.33581 (revision 33581) DartEditorEdgeCs now version 1.3.0-edge.33580 (revision 33580) DartEditorStable now version 1.2.0 (revision 33014) DartContentShellDev now version 1.3.0-dev.3.2 (revision 33495) DartContentShellEdge now version 1.3.0-edge.33581 (revision 33581) DartContentShellStable now version 1.2.0 (revision 33014)

--- dart-editor-edge.rb
+++ dart-editor-edge.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class DartEditorEdge < Formula
-  url "http://gsdview.appspot.com/dart-archive/channels/be/raw/33581/editor/darteditor-macos-x64.zip"
+  url "http://gsdview.appspot.com/dart-archive/channels/be/raw/33590/editor/darteditor-macos-x64.zip"
   homepage "http://www.dartlang.org/tools/editor/"
-  version "33581"
-  md5 "dddfd1e0eca58a5498e747406369709b"
+  version "33590"
+  md5 "xml"
   
   # conflicts_with 'dart-editor-dev', :because => 'installation of dart-dsk tools in path'
 # conflicts_with 'dart-editor-edge-cs', :because => 'installation of dart-dsk tools in path'
