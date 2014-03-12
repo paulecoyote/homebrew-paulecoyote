543795b3c2bde7f60c07835f0aa84d68ca0840ec
Paul Evans (Deploy) <paul.d.evans+dedeploy@gmail.com>
2014-03-12 13:00:13 +0000

    DartEditorDev now version 1.3.0-dev.3.2 (revision 33495) DartEditorEdge now version 1.3.0-edge.33571 (revision 33571) DartEditorEdgeCs now version 1.3.0-edge.33571 (revision 33571) DartEditorStable now version 1.2.0 (revision 33014) DartContentShellDev now version 1.3.0-dev.3.2 (revision 33495) DartContentShellEdge now version 1.3.0-edge.33571 (revision 33571) DartContentShellStable now version 1.2.0 (revision 33014)

--- dart-content-shell-edge.rb
+++ dart-content-shell-edge.rb
@@ -4,7 +4,7 @@ class DartContentShellEdge < Formula
   url "http://gsdview.appspot.com/dart-archive/channels/be/raw/33571/dartium/content_shell-macos-ia32-release.zip"
   homepage "http://www.dartlang.org/tools/editor/"
   version "33571"
-  md5 "xml"
+  md5 "4f72ff29ab9cb1d1609f7376819f14c7"
   
   # conflicts_with 'dart-content-shell-dev', :because => 'installation of dart-dsk tools in path'
 # conflicts_with 'dart-content-shell-stable', :because => 'installation of dart-dsk tools in path'
