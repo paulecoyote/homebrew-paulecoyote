4d71d4a9571e65f3bfcf2f09cbaf0c8f98c5cfbc
Paul Evans (Deploy) <paul.d.evans+dedeploy@gmail.com>
2014-03-27 11:00:13 +0000

    DartEditorDev now version 1.3.0-dev.6.1 (revision 34284). Zip: http://gsdview.appspot.com/dart-archive/channels/dev/release/34284/editor/darteditor-macos-x64.zip DartEditorEdge now version 1.3.0-edge.34466 (revision 34466). Zip: http://gsdview.appspot.com/dart-archive/channels/be/raw/34466/editor/darteditor-macos-x64.zip DartEditorEdgeCs now version 1.3.0-edge.34466 (revision 34466). Zip: http://gsdview.appspot.com/dart-archive/channels/be/raw/34466/editor/darteditor-macos-x64.zip DartEditorStable now version 1.2.0 (revision 33014). Zip: http://gsdview.appspot.com/dart-archive/channels/stable/release/33014/editor/darteditor-macos-x64.zip DartContentShellDev now version 1.3.0-dev.6.1 (revision 34284). Zip: http://gsdview.appspot.com/dart-archive/channels/dev/release/34284/dartium/content_shell-macos-ia32-release.zip DartContentShellEdge now version 1.3.0-edge.34466 (revision 34466). Zip: http://gsdview.appspot.com/dart-archive/channels/be/raw/34466/dartium/content_shell-macos-ia32-release.zip DartContentShellStable now version 1.2.0 (revision 33014). Zip: http://gsdview.appspot.com/dart-archive/channels/stable/release/33014/dartium/content_shell-macos-ia32-release.zip

--- dart-content-shell-edge.rb
+++ dart-content-shell-edge.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class DartContentShellEdge < Formula
-  url "http://gsdview.appspot.com/dart-archive/channels/be/raw/34465/dartium/content_shell-macos-ia32-release.zip"
+  url "http://gsdview.appspot.com/dart-archive/channels/be/raw/34466/dartium/content_shell-macos-ia32-release.zip"
   homepage "http://www.dartlang.org/tools/editor/"
-  version "34465"
-  md5 "bcd635c6ff193a03764d012e86963c3e"
+  version "34466"
+  md5 "bc2efc150f7e34e815c72363d13ad583"
   
   # conflicts_with 'dart-content-shell-dev', :because => 'installation of dart-dsk tools in path'
 # conflicts_with 'dart-content-shell-stable', :because => 'installation of dart-dsk tools in path'
