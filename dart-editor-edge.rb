953979047bcb6f952a47c1cc196bd43493abb5f8
Paul Evans (Deploy) <paul.d.evans+dedeploy@gmail.com>
2014-03-28 00:00:19 +0000

    DartEditorDev now version 1.3.0-dev.7.2 (revision 34463). Zip: http://gsdview.appspot.com/dart-archive/channels/dev/release/34463/editor/darteditor-macos-x64.zip DartEditorEdge now version 1.3.0-edge.34489 (revision 34489). Zip: http://gsdview.appspot.com/dart-archive/channels/be/raw/34489/editor/darteditor-macos-x64.zip DartEditorEdgeCs now version 1.3.0-edge.34483 (revision 34483). Zip: http://gsdview.appspot.com/dart-archive/channels/be/raw/34483/editor/darteditor-macos-x64.zip DartEditorStable now version 1.2.0 (revision 33014). Zip: http://gsdview.appspot.com/dart-archive/channels/stable/release/33014/editor/darteditor-macos-x64.zip DartContentShellDev now version 1.3.0-dev.7.2 (revision 34463). Zip: http://gsdview.appspot.com/dart-archive/channels/dev/release/34463/dartium/content_shell-macos-ia32-release.zip DartContentShellEdge now version 1.3.0-edge.34489 (revision 34489). Zip: http://gsdview.appspot.com/dart-archive/channels/be/raw/34489/dartium/content_shell-macos-ia32-release.zip DartContentShellStable now version 1.2.0 (revision 33014). Zip: http://gsdview.appspot.com/dart-archive/channels/stable/release/33014/dartium/content_shell-macos-ia32-release.zip

--- dart-editor-edge.rb
+++ dart-editor-edge.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class DartEditorEdge < Formula
-  url "http://gsdview.appspot.com/dart-archive/channels/be/raw/34486/editor/darteditor-macos-x64.zip"
+  url "http://gsdview.appspot.com/dart-archive/channels/be/raw/34489/editor/darteditor-macos-x64.zip"
   homepage "http://www.dartlang.org/tools/editor/"
-  version "34486"
-  md5 "166bad98177da5e627520314cd7c0265"
+  version "34489"
+  md5 "9197e5dc357313c06f6f1d4fc51ae752"
   
   # conflicts_with 'dart-editor-dev', :because => 'installation of dart-dsk tools in path'
 # conflicts_with 'dart-editor-edge-cs', :because => 'installation of dart-dsk tools in path'
