bdfe38613224adf3b5496fe1795798e1db139b2f
Paul Evans (Deploy) <paul.d.evans+dedeploy@gmail.com>
2014-06-21 01:00:09 +0000

    | Edition | Version | Revision | Archive | MD5 | Notes | | ------- | ------- | -------- | ------- | --- | ----- | | DartEditorDev | 1.5.0-dev.4.17 | 37532 | [Zip](http://storage.googleapis.com/dart-archive/channels/dev/release/37532/editor/darteditor-macos-x64.zip) | [md5](http://storage.googleapis.com/dart-archive/channels/dev/release/37532/editor/darteditor-macos-x64.zip.md5sum) | [Changes](http://storage.googleapis.com/dart-archive/channels/dev/release/latest/changelog.html) | | DartEditorEdge | 1.5.0-edge.37572 | 37572 | [Zip](http://storage.googleapis.com/dart-archive/channels/be/raw/37572/editor/darteditor-macos-x64.zip) | [md5](http://storage.googleapis.com/dart-archive/channels/be/raw/37572/editor/darteditor-macos-x64.zip.md5sum) | - | | DartEditorEdgeCs | 1.5.0-edge.37569 | 37569 | [Zip](http://storage.googleapis.com/dart-archive/channels/be/raw/37569/editor/darteditor-macos-x64.zip) | [md5](http://storage.googleapis.com/dart-archive/channels/be/raw/37569/editor/darteditor-macos-x64.zip.md5sum) | - | | DartEditorStable | 1.4.3 | 37348 | [Zip](http://storage.googleapis.com/dart-archive/channels/stable/release/37348/editor/darteditor-macos-x64.zip) | [md5](http://storage.googleapis.com/dart-archive/channels/stable/release/37348/editor/darteditor-macos-x64.zip.md5sum) | [Changes](http://storage.googleapis.com/dart-archive/channels/stable/release/latest/changelog.html) | | DartContentShellDev | 1.5.0-dev.4.17 | 37532 | [Zip](http://storage.googleapis.com/dart-archive/channels/dev/release/37532/dartium/content_shell-macos-ia32-release.zip) | [md5](http://storage.googleapis.com/dart-archive/channels/dev/release/37532/dartium/content_shell-macos-ia32-release.zip.md5sum) | - | | DartContentShellEdge | 1.5.0-edge.37569 | 37569 | [Zip](http://storage.googleapis.com/dart-archive/channels/be/raw/37569/dartium/content_shell-macos-ia32-release.zip) | [md5](http://storage.googleapis.com/dart-archive/channels/be/raw/37569/dartium/content_shell-macos-ia32-release.zip.md5sum) | - | | DartContentShellStable | 1.4.3 | 37348 | [Zip](http://storage.googleapis.com/dart-archive/channels/stable/release/37348/dartium/content_shell-macos-ia32-release.zip) | [md5](http://storage.googleapis.com/dart-archive/channels/stable/release/37348/dartium/content_shell-macos-ia32-release.zip.md5sum) | - |

--- dart-editor-edge.rb
+++ dart-editor-edge.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class DartEditorEdge < Formula
-  url "http://storage.googleapis.com/dart-archive/channels/be/raw/37571/editor/darteditor-macos-x64.zip"
+  url "http://storage.googleapis.com/dart-archive/channels/be/raw/37572/editor/darteditor-macos-x64.zip"
   homepage "http://www.dartlang.org/tools/editor/"
-  version "37571"
-  md5 "3996c01ef314b082eb98eda5333cb729"
+  version "37572"
+  md5 "324dea4aae190ddd75e6669ba67d160b"
   
   # conflicts_with 'dart-editor-dev', :because => 'installation of dart-dsk tools in path'
 # conflicts_with 'dart-editor-edge-cs', :because => 'installation of dart-dsk tools in path'
